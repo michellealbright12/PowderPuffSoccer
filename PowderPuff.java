f6a3d50a4e3e441063a89f9a075777483025235c
clmccart <clairelmccarthy14@gmail.com>
2017-12-04 21:20:09 -0500

    deleted getteammatedirection

--- PowderPuff.java
+++ PowderPuff.java
@@ -659,11 +659,4 @@ public class PowderPuff extends Player {
         }
         return BALL;
     }
-
-    /** Think about it. */
-    public int GetTeammateDirection(int n) {
-        return Parent.GetTeammateDirection(this, n);
-    }
-
-
 }
